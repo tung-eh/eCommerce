94c6b25af88154b691f5a5b717dab8fa6580f498
Tung Teng <tung.nguyen@employmenthero.com>
2019-03-17 14:37:33 +0700

    Add sample code to call API from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const App = () => <div>Hello world!</div>;
+const App = () => {
+  const [categoryCount, setCategoryCount] = useState();
+  const [productCount, setProductCount] = useState();
+  useEffect(() => {
+    fetch('/api/categories')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => setCategoryCount(data.length))
+      .catch(error => console.log(error));
+
+    fetch('/api/products')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => setProductCount(data.length))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div>Hello world!</div>
+      {categoryCount && productCount ? (
+        <div>
+          We have {categoryCount} categories and {productCount} products in our
+          database
+        </div>
+      ) : (
+        <div>Fetching data ....</div>
+      )}
+    </>
+  );
+};
 
 export default App;
