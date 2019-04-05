e998cbdb589ba2b1d77f9a2035ed7912af7472ed
Tung Teng <tung.nguyen@employmenthero.com>
2019-04-05 23:30:11 +0700

    Remove Ant design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import {Router} from '@reach/router';
 import React from 'react';
-import {Layout, Breadcrumb} from 'antd';
 
 import About from './About';
 import AuthProvider from './AuthProvider';
@@ -10,32 +9,19 @@ import Navigator from './Navigator';
 import Sample from './Sample';
 import SignUp from './SignUp';
 
-const {Footer, Content} = Layout;
-
 const App = () => (
   <AuthProvider>
-    <Layout>
-      <Navigator />
-      <Content>
-        <Breadcrumb style={{margin: '16px 0'}}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div style={{background: '#fff', padding: 24, minHeight: 280}}>
-          <Router>
-            <Home path="/" />
-            <About path="about" />
-            <Sample path="sample" />
-            <Login path="login" />
-            <SignUp path="signup" />
-          </Router>
-        </div>
-      </Content>
-      <Footer style={{textAlign: 'center'}}>
-        Simle eCommerce ©2019 Created by Tung Teng
-      </Footer>
-    </Layout>
+    <Navigator />
+    <Router>
+      <Home path="/" />
+      <About path="about" />
+      <Sample path="sample" />
+      <Login path="login" />
+      <SignUp path="signup" />
+    </Router>
+    <div style={{textAlign: 'center'}}>
+      Simle eCommerce ©2019 Created by Tung Teng
+    </div>
   </AuthProvider>
 );
 
--- client/src/components/Navigator/index.js
+++ client/src/components/Navigator/index.js
@@ -1,29 +1,26 @@
-import {Layout, Menu, Icon} from 'antd';
 import styled from 'styled-components';
 import React from 'react';
 import {Link, Location} from '@reach/router';
 
 import logo from '../../../assets/images/logo.png';
 
-const {Header, Footer, Sider, Content} = Layout;
-
-const StyledHeader = styled(Header)`
+const StyledHeader = styled.div`
   display: flex;
   padding: 0px;
+  background-color: #43d580;
+  height: 60px;
 `;
 
-const StyledMenu = styled(Menu)`
+const StyledMenu = styled.div`
   display: flex;
   align-items: center;
   justify-content: ${props =>
     ({left: 'flex-end', right: 'flex-start'}[props.align])};
   flex: 1;
-  line-height: 64px;
 `;
 
 const LogoLink = styled(Link)`
   height: 100%;
-  padding: 10px 20px;
   display: flex;
 `;
 
@@ -32,38 +29,17 @@ const Navigator = () => {
     <Location>
       {({location}) => (
         <StyledHeader>
-          <StyledMenu
-            mode="horizontal"
-            selectedKeys={[location.pathname]}
-            align="left">
-            <Menu.Item key="/">
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="/products">
-              <Link to="products">Products</Link>
-            </Menu.Item>
+          <StyledMenu align="left">
+            <Link to="/">Home</Link>
+            <Link to="products">Products</Link>
           </StyledMenu>
           <LogoLink to="">
             <img src={logo} style={{height: '100%'}} />
           </LogoLink>
-          <StyledMenu
-            mode="horizontal"
-            selectedKeys={[location.pathname]}
-            align="right">
-            <Menu.Item key="/about">
-              <Link to="about">About</Link>
-            </Menu.Item>
-            <Menu.Item key="/sample">
-              <Link to="sample">Sample</Link>
-            </Menu.Item>
+          <StyledMenu align="right">
+            <Link to="about">About</Link>
+            <Link to="sample">Sample</Link>
           </StyledMenu>
-          <Menu
-            mode="horizontal"
-            style={{display: 'none', position: 'absolute'}}>
-            <Menu.Item key="3">
-              <Icon type="menu" />
-            </Menu.Item>
-          </Menu>
         </StyledHeader>
       )}
     </Location>
